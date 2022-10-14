29d224c1ffdc2ed053928046bcc5994918f91115
liushuaifan <89621842+liushuaifan@users.noreply.github.com>
2022-10-14 13:54:24 -0500

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="nav">
+
+  </div>
+  <router-view/>
+</template>
+
+<script>
+ export default{
+   mounted(){
+
+
+   }
+ }
+</script>
+
+
+ <!--<style  lang="scss">
+#nav{
+  padding:10px;
+  width:100%;
+  height:30px;
+  background-color:white;
+  line-height:30px;
+
+  a{
+    font-weight:bold;
+    color:darkgray;
+    text-decoration:none;
+    margin:0 5px 0 5px;
+    font-size:1.25rem;
+
+    &.active{
+      color:#2c3e50;
+    }
+  }
+
+}
+</style>
+
+-->
+
+
+
+
